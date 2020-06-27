de3008bf63ca7d1f79ee7aa9ba7b2554343d816e
bfecchio <bruno.fecchio@gmail.com>
2020-06-27 17:50:25 -0300

    Implementacao dos testes unitários e integrados de Models\Genre

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -19,7 +19,18 @@ class CategoryTest extends TestCase
         $this->assertCount(1, $categories);
 
         $categoryKeys = array_keys($categories->first()->getAttributes());
-        $this->assertEqualsCanonicalizing(['id', 'name', 'description', 'is_active', 'created_at', 'updated_at', 'deleted_at'], $categoryKeys);
+        $this->assertEqualsCanonicalizing(
+            [
+                'id',
+                'name',
+                'description',
+                'is_active',
+                'created_at',
+                'updated_at',
+                'deleted_at'
+            ],
+            $categoryKeys
+        );
     }
 
     public function testCreate()
@@ -29,6 +40,7 @@ class CategoryTest extends TestCase
         ]);
         $category->refresh();
 
+        $this->assertEquals(36, strlen($category->id));
         $this->assertEquals('test1', $category->name);
         $this->assertNull($category->description);
         $this->assertTrue($category->is_active);
@@ -58,7 +70,7 @@ class CategoryTest extends TestCase
         $this->assertTrue($category->is_active);
     }
 
-    public function testeUpdate()
+    public function testUpdate()
     {
         $category = factory(Category::class)->create([
             'description' => 'test_description',
@@ -75,5 +87,15 @@ class CategoryTest extends TestCase
         foreach($data as $key => $value) {
             $this->assertEquals($value, $category->{$key});
         }
+    }
+
+    public function testDelete() {
+        $category = factory(Category::class)->create();
+
+        $category->delete();
+        $this->assertNull(Category::find($category->id));
+
+        $category->restore();
+        $this->assertNotNull(Category::find($category->id));
     }
 }
