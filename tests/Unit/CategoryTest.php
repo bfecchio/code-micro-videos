9b7b474d24a11d1470d8ecd6311b4c43f063725e
bfecchio <bruno.fecchio@gmail.com>
2020-06-26 22:11:13 -0300

    Imlementação do metodo de setUp no testes de PHPUnit

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -16,47 +16,44 @@ class CategoryTest extends TestCase
 {
     use DatabaseMigrations;
     
-    public function testIfUseTraits()
+    private $category;
+
+    protected function setUp(): void
     {
-        Genre::create(['name' => 'test']);
+        parent::setUp();
+        $this->category = new Category();
+    }    
+
+    public function testIfUseTraits()
+    {        
         $traits = [SoftDeletes::class, Uuid::class];
         $categoryTraits = array_keys(class_uses(Category::class));
-
         $this->assertEquals($traits, $categoryTraits);
     }
 
     public function testFillableAttribute()
     {
-        $fillable = ['name', 'description', 'is_active'];
-        $category = new Category();
-
-        $this->assertEquals($fillable, $category->getFillable());
+        $fillable = ['name', 'description', 'is_active'];                
+        $this->assertEquals($fillable, $this->category->getFillable());
     }
 
     public function testDatesAttribute()
     {
-        $dates = ['created_at', 'updated_at', 'deleted_at'];
-        $category = new Category();
-
+        $dates = ['created_at', 'updated_at', 'deleted_at'];        
         foreach($dates as $date) {
-            $this->assertContains($date, $category->getDates());
-        }   
-        
-        $this->assertCount(count($dates), $category->getDates());
+            $this->assertContains($date, $this->category->getDates());
+        }           
+        $this->assertCount(count($dates), $this->category->getDates());
     }
 
     public function testCastsAttribute()
     {
-        $casts = ['id' => 'string'];
-        $category = new Category();
-
-        $this->assertEquals($casts, $category->getCasts());
+        $casts = ['id' => 'string'];        
+        $this->assertEquals($casts, $this->category->getCasts());
     }    
 
     public function testIncrementingAttribute()
     {        
-        $category = new Category();
-
-        $this->assertFalse($category->incrementing);
+        $this->assertFalse($this->category->incrementing);
     }        
 }
