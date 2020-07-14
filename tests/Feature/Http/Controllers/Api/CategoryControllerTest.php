034627beff9029ae3b8f19af2b8b1c72e473da00
bfecchio <bruno.fecchio@gmail.com>
2020-07-14 20:23:12 -0300

    Refatoração dos testes de Categoria

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Feature\Http\Controllers\Api;
 
+use Tests\TestCase;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\TestCase;
 
 use Tests\Traits\TestSaves;
 use Tests\Traits\TestValidations;
@@ -67,61 +67,23 @@ class CategoryControllerTest extends TestCase
 
     public function testUpdate()
     {
-        $category = factory(Category::class)->create([
+        $this->category = factory(Category::class)->create([
             'description' => 'description',
             'is_active' => false
         ]);
-        $response = $this->json(
-            'PUT',
-            route('categories.update', ['category' => $category->id]),
-            [
-                'name' => 'test',
-                'description' => 'test',
-                'is_active' => true
-            ]
-        );
 
-        $id = $response->json('id');
-        $category = Category::find($id);
+        $data = ['name' => 'test', 'description' => 'test', 'is_active' => true];
+        $response = $this->assertUpdate($data, $data + ['deleted_at' => null]);
+        $response->assertJsonStructure(['created_at', 'updated_at']);
 
-        $response
-            ->assertStatus(200)
-            ->assertJson($category->toArray())
-            ->assertJsonFragment([
-                'description' => 'test',
-                'is_active' => true
-            ]);
+        $data = ['name' => 'test', 'description' => ''];
+        $this->assertUpdate($data, array_merge($data, ['description' => null]));
 
-        $response = $this->json(
-            'PUT',
-            route('categories.update', ['category' => $category->id]),
-            [
-                'name' => 'test',
-                'description' => ''
-            ]
-        );   
-        
-        $response            
-            ->assertJsonFragment([
-                'description' => null
-            ]);  
-                        
-        $category->description = 'test';
-        $category->save();
+        $data['description'] = 'test';
+        $this->assertUpdate($data, array_merge($data, ['description' => 'test']));
 
-        $response = $this->json(
-            'PUT',
-            route('categories.update', ['category' => $category->id]),
-            [
-                'name' => 'test',
-                'description' => null
-            ]
-        );   
-        
-        $response            
-            ->assertJsonFragment([
-                'description' => null
-            ]);          
+        $data['description'] = null;
+        $this->assertUpdate($data, array_merge($data, ['description' => null]));                              
     }  
     
     public function testDestroy()
--- tests/Traits/TestSaves.php
+++ tests/Traits/TestSaves.php
@@ -16,13 +16,36 @@ trait TestSaves
             throw new \Exception("Response status must be 201, given {$response->status()}:\n{$response->content()}");
         }
 
+        $this->assertInDatabase($response, $testDatabase);
+        $this->assertJsonResponseContent($response, $testDatabase, $testJsonData);
+        
+        return $response;
+    }
+
+    protected function assertUpdate($sendData, array $testDatabase, array $testJsonData = null): TestResponse
+    {
+        /** @var TestResponse $response */
+        $response = $this->json('PUT', $this->routeUpdate(), $sendData);
+        if ($response->status() !== 200){
+            throw new \Exception("Response status must be 200, given {$response->status()}:\n{$response->content()}");
+        }
+
+        $this->assertInDatabase($response, $testDatabase);
+        $this->assertJsonResponseContent($response, $testDatabase, $testJsonData);
+        
+        return $response;
+    }
+
+    private function assertInDatabase(TestResponse $response, array $testDatabase)
+    {
         $model = $this->model();
         $table = (new $model)->getTable();
         $this->assertDatabaseHas($table, $testDatabase + ['id' => $response->json('id')]);
+    }
 
+    private function assertJsonResponseContent(TestResponse $response, array $testDatabase, array $testJsonData = null)
+    {
         $testResponse = $testJsonData ?? $testDatabase;
         $response->assertJsonFragment($testResponse + ['id' => $response->json('id')]);
-
-        return $response;
     }
 }
